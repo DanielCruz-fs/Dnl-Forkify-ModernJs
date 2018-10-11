caf8709fe4f053a512f85ac1137e97cd1513808f
unknown <dannyfsociety@gmail.com>
2018-10-10 20:39:07 -0400

    Initial Commit Boiler Plate

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import x from './test';
+const number = 23;
+console.log(`it is imported ${x} and the number es6 variable is: ${number}`);
---
+++ src/js/test.js
@@ -0,0 +1,2 @@
+console.log('this is module that\'s been imported');
+export default 99;
